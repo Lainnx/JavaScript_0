71d1418817b675e40848eaff5c2dfe324309bd5f
das <tree@gmail.com>
2025-03-27 18:50:46 +0100

    s

--- 03_apuntes_javascript/12_main.js
+++ 03_apuntes_javascript/12_main.js
@@ -3,14 +3,15 @@ import texto from './12_texto.js'
 console.log(texto);
 
 const btnTexto = document.getElementById("btnTexto")
+const divTexto = document.getElementById("divTexto")
 
 btnTexto.addEventListener("click",()=>{
 
     fetch("./12_texto.txt")
     .then(data =>{
-        data.text() //pasamos data a texto (porque data es texto, pero cuando lo lee es dato y hay que convertirlo)
+        return data.text() //pasamos data a texto (porque data es texto, pero cuando lo lee es dato y hay que convertirlo)
     })
-    .then(data =>{
+    .then(data => {
         console.log(data);
         divTexto.textContent = data;
     })
@@ -20,4 +21,19 @@ btnTexto.addEventListener("click",()=>{
     .finally(()=>{  //se va a ejecutar tanto si funciona como si no(tanto si hay errores como si no)
         console.log("programa finalizado");
     })
+})
+
+const btnDatos = document.getElementById("btnDatos")
+const divDatos = document.getElementById("divDatos")
+
+btnDatos.addEventListener("click",()=>{
+    fetch("./12_datos.json")
+    .then(data=>{
+        // console.log(data);
+        return data.json()  //cuando pones llaves tienes que poner el return
+    })
+    .then(data=>{
+        console.log(data);
+    }).catch(error =>console.error("Error",error))
+    .finally(()=>{console.log("finalizar");})
 })
---
+++ 09_app_meteo/js/app.js
@@ -0,0 +1,40 @@
+import key from "./key.js"
+// console.log(key);
+
+const formCiudad = document.forms["formCiudad"]
+
+formCiudad.addEventListener("submit",(e)=>{
+    e.preventDefault()
+
+    const ciudad=formCiudad["nombreCiudad"].value
+    const appid = key
+    const idioma = "es"
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?appid=${appid}&units=metric&lang=${idioma}&q=${ciudad}`
+  
+   
+    const divDatos = document.getElementById("divDatos")
+    
+    fetch(url)
+    .then(data=>data.json())  //pedimos que el data lo pase a json
+    .then(data=>{
+        divDatos.innerHTML=""
+        divDatos.innerHTML += `<p>Ciudad: ${data.name}</p>`
+        divDatos.innerHTML+=`<p>Temperatura actual: ${data["main"]["temp"]}ºC</p>`  //se podria con puntos pero si la ciudad lleva espacios puede dar problemas
+        divDatos.innerHTML+=`<p>Descripción: ${data["weather"][0]["description"]}</p>`//dentro de weather, luego pos 0(es un array), elemento "descripcion"
+        divDatos.innerHTML+=`<div><img src="https://www.imelcf.gob.pa/wp-content/plugins/location-weather/assets/images/icons/weather-icons/${data["weather"][0]["icon"]}.svg"></div>`
+    
+    
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
